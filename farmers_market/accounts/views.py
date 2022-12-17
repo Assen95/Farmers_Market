169c72d34270a1a961e252741a15f39b38b8286d
Assen95 <asenjob95@gmail.com>
2022-12-17 20:56:01 +0200

    Sixth Commit

--- farmers_market/accounts/views.py
+++ farmers_market/accounts/views.py
@@ -39,6 +39,7 @@ class UserDetailsView(views.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_owner'] = self.request.user = self.object
+        context['has_company'] = Company.objects.filter(user_id=self.request.user.pk).get()
         return context
 
 
@@ -54,9 +55,20 @@ class UserEditView(views.UpdateView):
             'pk': self.request.user.pk,
         })
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_owner'] = self.request.user = self.object
+        context['has_company'] = Company.objects.filter(user_id=self.request.user.pk).get()
+        return context
+
 
 # Deletes user
 class UserDeleteView(views.DeleteView):
     template_name = 'profile/delete-profile.html'
     model = UserModel
     success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['has_company'] = Company.objects.filter(user_id=self.request.user.pk).get()
+        return context
